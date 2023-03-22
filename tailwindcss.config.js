51cf5875dcfa065385cd54db07e69d5dc2f142d1
Christophe Kowarski <116869316+ChristKS@users.noreply.github.com>
2023-03-23 00:01:48 +1030

    Update tailwindcss.config.js

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 module.exports = {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
+        path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+        path.join(__dirname, './components/**/*.{js,ts,jsx,tsx}'),
         // For the best performance and to avoid false positives,
         // be as specific as possible with your content configuration.
     ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
