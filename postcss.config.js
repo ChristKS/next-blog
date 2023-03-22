103155c412200b3f79f1b4123d8bbbef18229068
Christophe Kowarski <116869316+ChristKS@users.noreply.github.com>
2023-03-23 00:02:12 +1030

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 module.exports = {
     plugins: {
-        tailwindcss: {},
+        tailwindcss: {
+            config: path.join(__dirname, 'tailwindcss.config.js'),
+        },
         autoprefixer: {},
     },
 };
